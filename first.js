0cd413bb87ec4d48936c04563db7498ad22cb760
Ayushman Dutta <im.ayushmandutta@gmail.com>
2025-07-13 23:44:29 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+let modeButton= document.querySelector("#mode");
+let currMode= "light"; //stores the current modee
+modeBtn.addEventListener("clicl",() =>{
+    if (currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body).")
+    }else {
+        currModde = "light"
+    }
+    console.log(currMode);
+   
+});
